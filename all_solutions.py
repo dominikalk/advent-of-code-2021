3a6678d86397148ba72c55465bad75d60f9135fa
Dominik Alkhovik <dominik.alk@hotmail.com>
2021-12-02 13:35:02 +0000

    add input for which solution

--- all_solutions.py
+++ all_solutions.py
@@ -1,5 +1,15 @@
 from day1.solution import main as day1_solution
 from day2.solution import main as day2_solution
 
-day1_solution()
-day2_solution()
+def main():
+    day_solutions = [day1_solution, day2_solution]
+    days_completed = len(day_solutions)
+    day_input = input(f"Which day would you like the solution to? (1 - {days_completed} else ALL): ")
+    if not day_input.isnumeric() or int(day_input) < 1 or int(day_input) > days_completed:
+        for day_solution in day_solutions:
+            day_solution()
+    else:
+        day_solutions[int(day_input) - 1]()
+
+if __name__ == "__main__":
+    main()
