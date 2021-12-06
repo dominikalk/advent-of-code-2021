3d3a28297d6e0830add0ea844e3a8d90f22dd1f3
Dominik Alkhovik <dominik.alk@hotmail.com>
2021-12-06 22:00:56 +0000

    finish day 6

--- day6/solution.py
+++ day6/solution.py
@@ -3,20 +3,32 @@ from os.path import exists
 def main():
     print("[DAY 6]")
 
-    puzzle_input = []
-
     if exists("day6/input.txt"):
         f = open("day6/input.txt", "r")
     elif exists("input.txt"):
         f = open("input.txt", "r")
     else:
         return print("An Error Has Occured")
-    puzzle_input = f.readline().strip().split(',')
+    puzzle_input = list(map(int, f.readline().strip().split(',')))
     f.close()
 
-    # print("[TASK 1]: " + str(int(gamma_rate, 2) * int(epsilon_rate, 2)))
+    # This way is much quicker than a nested loop as it has a complexity of O(n)
+    def quick_find_fish(days):
+        fish_stages = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+        for fish in puzzle_input:
+            fish_stages[fish] += 1
+        fish_stages = list(fish_stages.values())
 
-    # print("[TASK 2]: " + str(findListValue(puzzle_input, True, 0) * findListValue(puzzle_input, False, 0)))
+        for _ in range(days):
+            fish_stages_zero = fish_stages[0]
+            fish_stages = fish_stages[1:] + fish_stages[:1]
+            fish_stages[6] += fish_stages_zero
+
+        return sum(fish_stages)
+
+    print("[TASK 1]: " + str(quick_find_fish(80)))
+
+    print("[TASK 2]: " + str(quick_find_fish(256)))
 
 if __name__ == "__main__":
     main()
