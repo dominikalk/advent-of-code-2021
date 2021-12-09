ee52c13a9fe114a6288102adc7ac2ab831d1a165
Dominik Alkhovik <dominik.alk@hotmail.com>
2021-12-09 21:17:50 +0000

    finish day 9

--- all_solutions.py
+++ all_solutions.py
@@ -6,11 +6,12 @@ from day5.solution import main as day5_solution
 from day6.solution import main as day6_solution
 from day7.solution import main as day7_solution
 from day8.solution import main as day8_solution
-
-
+from day9.solution import main as day9_solution
 
 def main():
-    day_solutions = [day1_solution, day2_solution, day3_solution, day4_solution, day5_solution, day6_solution, day7_solution, day8_solution]
+    day_solutions = [day1_solution, day2_solution, day3_solution, day4_solution, day5_solution, day6_solution, day7_solution, 
+    day8_solution, day9_solution]
+    
     days_completed = len(day_solutions)
     day_input = input(f"Which day would you like the solution to? (1 - {days_completed} else ALL): ")
     if not day_input.isnumeric() or int(day_input) < 1 or int(day_input) > days_completed:
--- day9/solution.py
+++ day9/solution.py
@@ -18,6 +18,7 @@ def main():
         width = len(puzzle_input[0])
         height = len(puzzle_input)
         risk_level_sum = 0
+        bottom_basins = []
 
         for i in range(width):
             for v in range(height):
@@ -39,12 +40,49 @@ def main():
                         
                 if is_lowest:
                     risk_level_sum += int(puzzle_input[v][i]) + 1
+                    bottom_basins.append([i, v])
             
-        return risk_level_sum
+        return (risk_level_sum, bottom_basins)
 
-    print("[TASK 1]: " + str(calculate_risk_level()))
+    def calculate_basin(positions, new_positions):
+        width = len(puzzle_input[0])
+        height = len(puzzle_input)
 
-    # print("[TASK 2]: " + str(calculate_output_addition()))
+        next_new_positions = []
+        for new_pos in new_positions:
+            if new_pos[0] != 0 and puzzle_input[new_pos[1]][new_pos[0] - 1] != '9' and [new_pos[0] - 1, new_pos[1]] not in positions:
+                next_new_positions.append([new_pos[0] - 1, new_pos[1]])
+                positions.append([new_pos[0] - 1, new_pos[1]])
+            if new_pos[0] != width - 1 and puzzle_input[new_pos[1]][new_pos[0] + 1] != '9' and [new_pos[0] + 1, new_pos[1]] not in positions:
+                next_new_positions.append([new_pos[0] + 1, new_pos[1]])
+                positions.append([new_pos[0] + 1, new_pos[1]])
+            if new_pos[1] != 0 and puzzle_input[new_pos[1] - 1][new_pos[0]] != '9' and [new_pos[0], new_pos[1] - 1] not in positions:
+                next_new_positions.append([new_pos[0], new_pos[1] - 1])
+                positions.append([new_pos[0], new_pos[1] - 1])
+            if new_pos[1] != height - 1 and puzzle_input[new_pos[1] + 1][new_pos[0]] != '9' and [new_pos[0], new_pos[1] + 1] not in positions:
+                next_new_positions.append([new_pos[0], new_pos[1] + 1])
+                positions.append([new_pos[0], new_pos[1] + 1])
+
+        if len(next_new_positions) == 0:
+            return len(positions)
+        else:
+            return calculate_basin(positions, next_new_positions)
+
+    def calculate_top_basins():
+        basin_sizes = []
+        basin_positions = calculate_risk_level()[1]
+
+        for basin_position in basin_positions:
+            basin_sizes.append(calculate_basin([[basin_position[0], basin_position[1]]], [[basin_position[0], basin_position[1]]]))
+        
+        basin_sizes = sorted(basin_sizes, reverse=True)
+
+        return basin_sizes[0] * basin_sizes[1] * basin_sizes[2]
+                    
+
+    print("[TASK 1]: " + str(calculate_risk_level()[0]))
+
+    print("[TASK 2]: " + str(calculate_top_basins()))
 
 if __name__ == "__main__":
     main()
